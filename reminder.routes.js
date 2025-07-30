e5dee0ac4b38f85a4dd854c4636937a742ff87ae
paulodonadel <paulodonadel@gmail.com>
2025-07-30 15:16:07 -0300

    Update reminder.routes.js

--- reminder.routes.js
+++ reminder.routes.js
@@ -1,7 +1,6 @@
 
 const express = require('express');
 const { body } = require('express-validator');
-const router = express.Router();
 
 // Handler catch-all para logar requests não tratados
 router.use((req, res, next) => {
