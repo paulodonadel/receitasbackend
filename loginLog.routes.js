5fa1d56f921f0990f0b58ba1e27fc051fb500a00
paulodonadel <paulodonadel@gmail.com>
2026-03-02 21:36:48 -0300

    Fix: Correct middleware import path in loginLog.routes.js

--- loginLog.routes.js
+++ loginLog.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect, authorize } = require('./middleware/auth');
+const { protect, authorize } = require('./middlewares/auth.middleware');
 const {
   getLoginLogs,
   getLoginStats,
